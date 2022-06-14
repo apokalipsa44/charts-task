90b2e19fc773fc5ffe1f53b9f5c5988d085a3beb
apokalipsa44 <m_lachowicz@wp.pl>
2022-06-15 00:57:03 +0200

    change backgroun on refresh done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'charts-task';
+export class AppComponent implements OnInit {
+  isBackgrounVisible = false
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        distinctUntilChanged()
+      ).subscribe(refresh => {
+        let counter: number = (sessionStorage.getItem("refreshCounter")) ? parseInt(sessionStorage.getItem("refreshCounter")!) : 0;
+        counter++;
+        sessionStorage.setItem("refreshCounter", counter?.toString());
+        if (counter % 5 === 0) this.isBackgrounVisible = true;
+      })
+  }
 }
