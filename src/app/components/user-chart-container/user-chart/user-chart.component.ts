82ea8b34cb9233cea478b2b94c7c0b98ab1dd6f1
apokalipsa44 <m_lachowicz@wp.pl>
2022-06-14 22:15:43 +0200

    charts lib added

--- src/app/components/user-chart-container/user-chart/user-chart.component.ts
+++ src/app/components/user-chart-container/user-chart/user-chart.component.ts
@@ -1,7 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Label } from 'ng2-charts';
-import { Color } from 'ng2-charts/lib/color';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { User } from 'src/app/models/user.model';
 
 @Component({
@@ -11,23 +8,6 @@ import { User } from 'src/app/models/user.model';
 })
 export class UserChartComponent implements OnChanges {
   @Input() usersList!: User[] | null;
-
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: (ChartOptions & {responsive: true}) = {
-    responsive: true,
-  };
-  public lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
-  ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
-  public lineChartPlugins = [];
 
   constructor() { }
 
