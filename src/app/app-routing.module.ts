853530a5b80417a1bf17c69c0eaa91f125f3e8b6
apokalipsa44 <m_lachowicz@wp.pl>
2022-06-15 18:02:00 +0200

    hotfix: route wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
- 
   { path: '', loadChildren: () => import('./UI/main-view/main-view.module').then(m => m.MainViewModule) },
- 
-  // { path: 'main-view', loadChildren: () => import('./UI/main-view/main-view.module').then(m => m.MainViewModule) },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
